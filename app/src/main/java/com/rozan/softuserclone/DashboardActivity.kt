db4b20d46a5f51f1cd3eb86ad031d8d1d4677ea0
Rojan904 <57635028+Rojan904@users.noreply.github.com>
2021-01-14 22:25:09 +0545

    Completed navigation of fragments.

--- app/src/main/java/com/rozan/softuserclone/DashboardActivity.kt
+++ app/src/main/java/com/rozan/softuserclone/DashboardActivity.kt
@@ -1,20 +1,52 @@
 package com.rozan.softuserclone
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.app.ActionBar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.rozan.softuserclone.fragments.AboutFragment
+import com.rozan.softuserclone.fragments.HomeFragment
+import com.rozan.softuserclone.fragments.StudentFragment
 
 class DashboardActivity : AppCompatActivity() {
     private lateinit var toolbar:ActionBar
     private lateinit var bottomNavigation:BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
 
         toolbar= supportActionBar!!
         bottomNavigation=findViewById(R.id.bottomNavigation)
-
+        bottomNavigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+        openFragment(HomeFragment())
 
     }
+    private val mOnNavigationItemSelectedListener=BottomNavigationView.OnNavigationItemSelectedListener { item->
+        when(item.itemId){
+            R.id.navHome -> {
+                val homeFragment = HomeFragment.newInstance()
+                openFragment(homeFragment)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navAddStudent -> {
+                val studentFragment = StudentFragment.newInstance()
+                openFragment(studentFragment)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navAbout -> {
+                val aboutFragment = AboutFragment.newInstance()
+                openFragment(aboutFragment)
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
+    }
+
+    private fun openFragment(fragment: Fragment) {
+        val transaction=supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.container, fragment)
+        transaction.commit()
+}
 }
